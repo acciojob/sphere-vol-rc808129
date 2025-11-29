3a8e7e3e637b773263154fbc36617028cd5f84a1
Raj Chaurasiya <rc808129@gmail.com>
2025-11-29 19:10:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    let inputvalue = parseFloat(document.getElementById('radius').value);
+    let volumn = document.getElementById('volume');
+    
+    if (inputvalue < 0 || isNaN(inputvalue)) {
+        volumn.value = 'NaN';
+    } else {
+        let calculate = (4 / 3) * Math.PI * Math.pow(inputvalue, 3);
+        volumn.value = calculate.toFixed(4); // Rounding to 4 decimal places
+    }
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = function(event) {
+        event.preventDefault(); // Prevent form submission
+        volume_sphere();
+    };
+};
